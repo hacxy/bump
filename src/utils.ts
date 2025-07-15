b322fac803464bbf77bb55e374c98972a665be4f
hacxy <hacxy.js@outlook.com>
2025-07-15 21:06:13 +0800

    fix: remove internal changelog generator

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,5 @@
-import { readFileSync, statSync, writeFileSync } from 'node:fs';
+import { readFileSync, writeFileSync } from 'node:fs';
 import path from 'node:path';
-import { cwd } from 'node:process';
-import { ConventionalChangelog } from 'conventional-changelog';
 import { execa } from 'execa';
 import c from 'picocolors';
 import semver from 'semver';
@@ -30,35 +28,4 @@ export function updatePackage(version: string) {
 export const versionIncrements = ['patch', 'minor', 'major', 'beta', 'alpha'];
 
 export const tags = ['latest', 'next'];
-
-/**
- * changelog generator
- */
-export async function generateChangelog() {
-  const generator = new ConventionalChangelog()
-    .readPackage()
-    .loadPreset('angular');
-
-  const changelogPath = path.resolve(cwd(), 'CHANGELOG.md');
-
-  try {
-    statSync(changelogPath);
-  }
-  // eslint-disable-next-line unused-imports/no-unused-vars
-  catch (_) {
-    writeFileSync(changelogPath, '');
-  }
-
-  const existingContent = readFileSync(changelogPath, 'utf8');
-
-  let changelogData = '';
-  for await (const chunk of generator.write()) {
-    changelogData += chunk;
-  }
-
-  // 将新日志插入到顶部
-  const updatedContent = `${changelogData}\n\n${existingContent}`;
-
-  writeFileSync(changelogPath, updatedContent);
-}
 
